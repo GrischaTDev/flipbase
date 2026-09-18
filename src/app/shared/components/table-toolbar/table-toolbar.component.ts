85f28ffc4ef698e0cc828dd33c5b865648aad053
Grischa Tänzer <Grischa.Taenzer@gmail.com>
2026-09-19 00:09:49 +0200

    refactor(ui): remove legacy table toolbar

--- src/app/shared/components/table-toolbar/table-toolbar.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-table-toolbar',
-  templateUrl: './table-toolbar.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'block' },
-})
-export class TableToolbarComponent {}
